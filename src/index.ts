cf9ba4981e97e4f2631aa247aaba0652b9259acc
Igor Korchagin <igor.korchagin8@gmail.com>
2022-11-08 10:03:16 +0100

    feat: add spectral ops

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 export * from './sum';
 
-const cp = require('child_process');
-const path = require('path');
-
-export function cleanupTemp() {
-  const cmd = 'rm -rf ' + path.join(__dirname, 'temp');
-  cp.execSync(cmd); // BAD
-}
+export const API_KEY = '5pIefVMwMiOdxOirB1ZKzQdFaFqiaeB5';
