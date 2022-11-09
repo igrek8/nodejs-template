2240477fcde92d112ae843748e56bfa9b0adbb5a
Igor Korchagin <igor.korchagin8@gmail.com>
2022-11-09 09:04:27 +0100

    build: add codecov & bump version

--- release.config.js
+++ release.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            type: 'build',
+            scppe: 'deps',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     ['@semantic-release/npm', { npmPublish: false, tarballDir: './dist' }],
     ['@semantic-release/github', { assets: './dist/*.tgz' }],
